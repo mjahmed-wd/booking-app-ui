9bba9a49e66bb9aace819d793b4dc9bce2d837b4
Md Jubair Ahmed <mjahmed.wd@gmail.com>
2021-08-20 10:50:20 +0600

    travelCard btn done

--- src/components/BookingPage/BookingPage.js
+++ src/components/BookingPage/BookingPage.js
@@ -1,61 +1,22 @@
 import React from "react";
 import BookingSelection from "../BookingSelection/BookingSelection";
 import usAirLogo from "../../images/search/icn_Logo_American-Airlines.svg";
+import ShareIcon from "@material-ui/icons/Share";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import Debit from "../../images/search/Direct Debit.svg";
+import TravelCard from "../../helper/_travelCard";
 
 const BookingPage = () => {
+  const [hisStudet, setData] = [1, 2, 3, 4, 5, 6, 6];
   return (
     <div className="m-3">
       <BookingSelection />
-      <div
-        className="row"
-        style={{ border: "1px solid black", borderRadius: "5px" }}
-      >
-        <div className="m-2">
-          <span
-            style={{
-              color: "#2A7BE4",
-              backgroundColor: "#A0C4F3",
-              borderRadius: "5px",
-              padding: "0 10px",
-            }}
-          >
-            Best
-          </span>
-          <span
-            className="ms-2"
-            style={{
-              color: "#A8DDC7",
-              backgroundColor: "#E0FAEF",
-              borderRadius: "5px",
-              padding: "0 10px",
-            }}
-          >
-            Cheapest
-          </span>
-        </div>
-        <div
-          className="col-9 m-2 d_grid_4"
-          style={{ borderRight: "1px solid black" }}
-        >
-          <input type="checkbox" name="check1" id="" />
-          <div>
-            <img src={usAirLogo} alt="" className="mt-0" />
-            <img src={usAirLogo} alt="" className="mt-4" />
-          </div>
-         <div>
-             <h4>01:30 - 18:30</h4>
-             <p>SAW Sabiha Gokcen - SAN San Fransisco</p>
-         </div>
-         <div>
-             <b>2 Steps</b>
-             <p>DOH, SFO</p>
-         </div>
-         <div>
-             <b>27 hours</b>
-         </div>
-        </div>
-        <div className="col-3"></div>
-      </div>
+      <TravelCard />
+      <TravelCard />
+      <TravelCard />
+      <TravelCard />
+      <TravelCard />
+      <TravelCard />
     </div>
   );
 };
---
+++ src/helper/_travelCard.js
@@ -0,0 +1,108 @@
+import Debit from "../images/search/Direct Debit.svg";
+import ShareIcon from "@material-ui/icons/Share";
+import usAirLogo from "../images/search/icn_Logo_American-Airlines.svg";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+
+const TravelCard = () => {
+  return (
+    <div
+      className="row shadow-lg mb-5 bg-body rounded"
+      style={{ borderRadius: "5px" }}
+    >
+      <div className="m-2">
+        <span
+          style={{
+            color: "#2A7BE4",
+            backgroundColor: "#A0C4F3",
+            borderRadius: "5px",
+            padding: "0 10px",
+          }}
+        >
+          Best
+        </span>
+        <span
+          className="ms-2"
+          style={{
+            color: "#A8DDC7",
+            backgroundColor: "#E0FAEF",
+            borderRadius: "5px",
+            padding: "0 10px",
+          }}
+        >
+          Cheapest
+        </span>
+      </div>
+      <div className="col-9 m-2 d_grid_4">
+        <>
+          <div>
+            <input type="checkbox" name="check1" id="" />
+          </div>
+          <div>
+            <img src={usAirLogo} alt="" />
+            <img src={usAirLogo} alt="" className="ms-4" />
+          </div>
+          <div className="ms-5">
+            <b>01:30 - 18:30</b>
+            <p>SAW Sabiha Gokcen - SAN San Fransisco</p>
+          </div>
+          <div>
+            <b>2 Steps</b>
+            <p>DOH, SFO</p>
+          </div>
+          <div>
+            <b>27 hours</b>
+          </div>
+          {/* second one */}
+          <div>
+            <input type="checkbox" name="check1" id="" />
+          </div>
+          <div>
+            <img src={usAirLogo} alt="" />
+            <img src={usAirLogo} alt="" className="ms-4" />
+          </div>
+          <div className="ms-5">
+            <b>01:30 - 18:30</b>
+            <p>SAW Sabiha Gokcen - SAN San Fransisco</p>
+          </div>
+          <div>
+            <b>2 Steps</b>
+            <p>DOH, SFO</p>
+          </div>
+          <div>
+            <b>27 hours</b>
+          </div>
+        </>
+      </div>
+      <div className="col-auto p-2">
+        <div type="">
+          <img src={Debit} alt="" srcset="" />
+          <h1>$ 249</h1>
+          <p>Qatar Airways</p>
+          <button className="btn-primary ps-5 pe-5" style={{ borderRadius: "5px" }}>
+            View Deal
+          </button>
+        </div>
+      </div>
+      <div
+        className="col-12 p-2"
+        style={{
+          backgroundColor: "#F9FAFD",
+          borderBottomLeftRadius: "5px",
+          borderBottomRightRadius: "5px",
+        }}
+      >
+        <div className="d-flex justify-content-between">
+          <p style={{ fontWeight: "500" }}>
+            Qatar Airways, Alaska Airlines <span>• Codeshares available</span>{" "}
+          </p>
+          <div className="pe-3">
+            <ShareIcon className="me-5" />
+            <FavoriteBorderIcon className="me-5" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TravelCard;
