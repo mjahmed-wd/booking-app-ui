e6ba0ef3edd863347b5bb4d4ca2238b02b1a62b1
Jubair <mjahmed.wd@gmail.com>
2021-08-21 16:07:38 +0600

    carosul fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,41 +1,43 @@
 import React from "react";
-// import React, { useEffect } from "react";
-// import ISelect from "../../helper/_select";
 import Maldivs from "./../../images/home/Rectangle 262a.png";
 import India from "./../../images/home/india.png";
 import London from "./../../images/home/london.png";
 import Arab from "./../../images/home/arab.png";
 import CardImage from "../../helper/_cardImage";
 import Carousel from "react-multi-carousel";
-// import SelectInput from "../../helper/_dateSelection";
-// import { Formik } from "formik";
-// import { useState } from "react";
-// import SearchIcon from "@material-ui/icons/Search";
 import BookingSelection from "../BookingSelection/BookingSelection";
 
-// const options = [
-//   { value: "0", label: "Dhaka" },
-//   { value: "1", label: "San Fransisco" },
-//   { value: "2", label: "New York" },
-// ];
-
 const Home = () => {
-  // const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  // const [fromDateAttr, setFromDateAttr] = useState("");
-  // const [toDateAttr, setToDateAttr] = useState("");
-  // const todayDateFormat = `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`;
-  // const changeDateAttr = (date, setter) => {
-  //   const weekDay = new Date(date).getDay();
-  //   const day = new Date(date).getDate();
-  //   const month = new Date(date).getMonth();
-  //   // console.log(date,weekdays[weekDay])
-  //   setter(`${weekdays[weekDay]} ${day}/${month}`);
-  // };
-  // useEffect(() => {
-  //   changeDateAttr(new Date(), setFromDateAttr);
-  //   changeDateAttr(new Date(), setToDateAttr);
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  const carousel = [
+    {
+      img: London,
+      title: `Delhi Events 2022`,
+      subTitle: `A small description goes here`,
+      topText: `Open`,
+      bottomText: `Important`,
+    },
+    {
+      img: Maldivs,
+      title: `Delhi Events 2022`,
+      subTitle: `A small description goes here`,
+      topText: `Open`,
+      bottomText: `Important`,
+    },
+    {
+      img: India,
+      title: `Delhi Events 2022`,
+      subTitle: `A small description goes here`,
+      topText: `Open`,
+      bottomText: `Important`,
+    },
+    {
+      img: London,
+      title: `Delhi Events 2022`,
+      subTitle: `A small description goes here`,
+      topText: `Open`,
+      bottomText: `Important`,
+    },
+  ];
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -110,34 +112,15 @@ const Home = () => {
               dotListClass="custom-dot-list-style"
               itemClass="carousel-item-padding-40-px"
             >
-              <CardImage
-                img={Maldivs}
-                title={`Delhi Events 2022`}
-                subTitle={`A small description goes here`}
-                topText={`Open`}
-                bottomText={`Important`}
-              />
-              <CardImage
-                img={India}
-                title={`Delhi Events 2022`}
-                subTitle={`A small description goes here`}
-                topText={`Open`}
-                bottomText={`Important`}
-              />
-              <CardImage
-                img={London}
-                title={`Delhi Events 2022`}
-                subTitle={`A small description goes here`}
-                topText={`Open`}
-                bottomText={`Important`}
-              />
-              <CardImage
-                img={Arab}
-                title={`Delhi Events 2022`}
-                subTitle={`A small description goes here`}
-                topText={`Open`}
-                bottomText={`Important`}
-              />
+              {carousel?.map((item, index) => (
+                <CardImage
+                  img={item?.img}
+                  title={item?.title}
+                  subTitle={item?.subTitle}
+                  topText={item?.topText}
+                  bottomText={item?.bottomText}
+                />
+              ))}
             </Carousel>
           </div>
         </div>
